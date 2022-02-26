989765891df5650bed9889b73b5bd83e0969624b
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-02-26 10:30:14 +0700

    create PostFactory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "title" => $this->faker->sentence(mt_rand(2, 8)),
+            "slug"  => $this->faker->slug(),
+            "description"   => $this->faker->paragraph(),
+            "body"  => $this->faker->paragraph(mt_rand(5, 10)),
+            "user_id"  => mt_rand(1, 5), // from total users that generated in DatabaseSeeder.php
+            "category_id"   => mt_rand(1, 2) // from total categories that generated in DatabaseSeeder.php
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,13 +18,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        User::create([
-            "name"  => "Muhammad Pauzi",
-            "email" => "muhammadpauzi@gmail.com",
-            "password"  => bcrypt("12345"),
-        ]);
-
+        User::factory(5)->create();
         Category::create([
             "name"  => "Web Programming",
             "slug"  => "web-programming"
@@ -34,21 +28,6 @@ class DatabaseSeeder extends Seeder
             "slug"  => "personal"
         ]);
 
-        Post::create([
-            "title" => "First Blog Post",
-            "slug"  => "first-blog-post",
-            "description"   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum veniam ab pariatur perspiciatis nihil mollitia laboriosam aspernatur soluta aperiam sed vel voluptatibus facilis.",
-            "body"  => "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum veniam ab pariatur perspiciatis nihil mollitia laboriosam aspernatur soluta aperiam sed vel voluptatibus facilis, asperiores maiores. Esse ad corrupti nam reiciendis unde voluptas. Facere libero aperiam nostrum tempore sint accusamus sequi voluptatem rem molestias. In labore harum esse quo libero.</p>",
-            "category_id"   => 1,
-            "user_id"   => 1
-        ]);
-        Post::create([
-            "title" => "Second Blog Post",
-            "slug"  => "second-blog-post",
-            "description"   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum veniam ab pariatur perspiciatis nihil mollitia laboriosam aspernatur soluta aperiam sed vel voluptatibus facilis.",
-            "body"  => "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum veniam ab pariatur perspiciatis nihil mollitia laboriosam aspernatur soluta aperiam sed vel voluptatibus facilis, asperiores maiores. Esse ad corrupti nam reiciendis unde voluptas. Facere libero aperiam nostrum tempore sint accusamus sequi voluptatem rem molestias. In labore harum esse quo libero.</p>",
-            "category_id"   => 2,
-            "user_id"   => 1
-        ]);
+        Post::factory(20)->create();
     }
 }
